ff74c4e19d1c77083888a435e46ea59e7780b293
Aakash R <aakashr2308@gmail.com>
2025-02-21 11:37:21 +0530

    Update App.jsx

--- Main/src/App.jsx
+++ Main/src/App.jsx
@@ -27,3 +27,21 @@ function App() {
 }
 
 export default App
+
+// import { initializeApp } from "firebase/app";
+// import { getAuth } from "firebase/auth";
+// import { getFirestore } from "firebase/firestore";
+
+// const firebaseConfig = {
+//   apiKey: "",
+//   authDomain: "",
+//   projectId: "",
+//   storageBucket: "",
+//   messagingSenderId: "",
+//   appId: "",
+//   measurementId: ""
+// };
+
+// const app = initializeApp(firebaseConfig);
+// export const auth = getAuth(app);
+// export const db = getFirestore(app);
