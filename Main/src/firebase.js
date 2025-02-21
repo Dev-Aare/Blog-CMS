4ddea844fd6206ce6434374bd5c2703b8ca57ebe
Aakash R <aakashr2308@gmail.com>
2025-02-21 10:53:18 +0530

    Update firebase.js

--- Main/src/firebase.js
+++ Main/src/firebase.js
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDFUEJM38SgRxxH1fdxfsdqiPbm3lX2pAU",
-  authDomain: "sinms-77bb8.firebaseapp.com",
-  projectId: "sinms-77bb8",
-  storageBucket: "sinms-77bb8.firebasestorage.app",
-  messagingSenderId: "1016967439935",
-  appId: "1:1016967439935:web:6326032ee2ebfaf4c4fa8a",
-  measurementId: "G-Y4J0EP02FD"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 const app = initializeApp(firebaseConfig);
